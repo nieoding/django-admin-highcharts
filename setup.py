143fb406c9d69df93aaf8ee025518e74c5969371
nieoding <dingzh@66wifi.com>
2017-04-18 09:49:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-admin-highcharts',
-    version='0.1',
+    version='0.2',
     packages=['admin_highcharts'],
     zip_safe=False,
     include_package_data=True,
